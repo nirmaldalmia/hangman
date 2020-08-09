af2421d0164b71ded4ff397923c0f9b321eab112
nirmaldalmia <nirmaldalmia17@gmail.com>
2020-08-09 23:14:02 +0530

    Updates

---
+++ precache-manifest.88c8cb9491b0501a34d2dd165621e2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a498824c9f881577ca8014c84624b117",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "f3823183ce8aed52c2ef",
+    "url": "/hangman/static/css/main.33b66289.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3823183ce8aed52c2ef",
+    "url": "/hangman/static/js/main.962b6769.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
